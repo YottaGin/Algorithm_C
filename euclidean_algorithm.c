47b283d8eff471bcf4f1f4d787b0c4c1f9828bd3
YottaGin <47880604+YottaGin@users.noreply.github.com>
2022-07-06 15:02:11 +0900

    Add Euclidean algorithm.

---
+++ euclidean_algorithm.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int get_gcd(int x, int y) {
+    // when a = bq + r, gcd(a, b) = gcd(b, r) while (r != 0)
+    int a, b, r;
+    if (x >= y) {
+        a = x; b = y;
+    } else {
+        a = y; b = x;
+    }
+    
+    r = a % b;
+    while (r != 0) {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    
+    return b;
+}
+
+int main(void){
+    char buf[1024];
+    char str[1024];
+    char *p;
+    int a, b;
+    
+    fgets(buf, sizeof(buf), stdin);
+    sscanf(buf, "%[^\n]", str);
+    p = strtok(str, " "); a = atoi(p);
+    p = strtok(NULL, " "); b = atoi(p);
+    
+    printf("%d\n", get_gcd(a, b));
+
+    return 0;
+}
